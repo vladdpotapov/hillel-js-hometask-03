4aa6427b8a17bf88957ef97eaaf3db82801bf562
PerSick <vlad461@ukr.net>
2020-01-14 01:29:51 +0300

    Task #05 - done

---
+++ js/Task_05.js
@@ -0,0 +1,10 @@
+// TASK #05:
+// Найти произведение всех целых чисел от 15 до 35.
+
+let product = 1;
+
+for (let number = 15; number <= 35; number++) {
+    product *= number;
+}
+
+document.write(product);
