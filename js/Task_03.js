fa1bb702518bf5786da890563a4019a4d6e22c9c
PerSick <vlad461@ukr.net>
2020-01-13 23:31:01 +0300

    Task #03 - done

---
+++ js/Task_03.js
@@ -0,0 +1,7 @@
+// TASK #03:
+// Вывести таблицу умножения на 7.
+
+for (let multiplier = 1, number = 7, result; multiplier <= 10; multiplier++) {
+    result = number * multiplier;
+    document.write(result + ' | ');
+}
