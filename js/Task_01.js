b2acd8bd2b96ef80a7bf942c3912cb8d209479f9
PerSick <vlad461@ukr.net>
2020-01-13 23:18:15 +0300

    Task #02 - done

--- js/Task_01.js
+++ js/Task_01.js
@@ -1,6 +1,6 @@
 // TASK #01:
 // Вывести на страницу в одну строку через запятую числа от 10 до 20.
 
-for (let number = 10; number >= 10 && number <= 20; number++) {
-    document.write(number + ', ');
+for (let number = 10; number <= 20; number++) {
+    document.write(number + ' | ');
 }
---
+++ js/Task_02.js
@@ -0,0 +1,7 @@
+// TASK #02:
+// Вывести квадраты чисел от 10 до 20.
+
+for (let digit = 10, result; digit <= 20; digit++) {
+    result = digit * digit;
+    document.write(result + ' | ');
+}
