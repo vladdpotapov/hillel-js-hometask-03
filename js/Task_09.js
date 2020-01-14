5fa31aaae2804e4b3e979307575d4fa1160fbf3b
PerSick <vlad461@ukr.net>
2020-01-14 09:37:28 +0300

    Task #09 - done

---
+++ js/Task_09.js
@@ -0,0 +1,32 @@
+// TASK #09:
+// Дано натуральное число. 
+// - найти и вывести на страницу все его делители. 
+// - определить количество его четных делителей;
+// - найти сумму его четных делителей.
+
+let userNumber = parseInt(prompt('Enter the number'));
+
+let divider;
+let dividerEven;
+let sum = 0;
+
+for (let counter = 1; counter <= userNumber; counter++) {
+    divider = userNumber / counter;
+
+    if (parseInt(divider) === divider) {
+        console.log('Dividers: ' + divider);
+    } 
+
+    if (divider % 2 === 0) {
+        dividerEven = divider;
+        console.log('Even divider: ' + dividerEven);
+        sum += dividerEven;
+    }   
+}
+
+console.log('Sum of dividers: ' + sum);
+
+
+
+
+
